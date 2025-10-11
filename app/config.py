0a9c3a22a81f916165067a8145de31a1368da306
NanezX <victor.nanej@gmail.com>
2025-10-11 17:39:38 -0400

    init config

---
+++ app/config.py
@@ -0,0 +1,23 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    # Database
+    DATABASE_URL: str
+
+    # Security
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    # App
+    PROJECT_NAME: str = "Optica API"
+    VERSION: str = "1.0.0"
+    API_V1_STR: str = "/api/v1"
+
+    class Config:
+        env_file = ".env"
+        case_sensitive = True
+
+
+settings = Settings()  # pyright: ignore
