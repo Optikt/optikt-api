56c757cf6e1189d1b449cdd6f6d273a197de9b81
NanezX <victor.nanej@gmail.com>
2025-11-01 11:40:20 -0400

    move to use argon to hashing password

--- app/core/security.py
+++ app/core/security.py
@@ -4,8 +4,7 @@ from passlib.context import CryptContext
 from jose import JWTError, jwt
 from app.config import settings
 
-# Configurar bcrypt para hacer hash de las contraseñas
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
