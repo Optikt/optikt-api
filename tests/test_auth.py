6ca33f8c59441afca1b11119a33a7da0a9737f99
NanezX <victor.nanej@gmail.com>
2025-11-01 13:05:28 -0400

    initial tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,13 +1,8 @@
-from fastapi.testclient import TestClient
-from app.main import app
+def test_health_check(client):
+    response = client.get("/health")
 
-
-client = TestClient(app)
-
-
-def test_read_main():
-    response = client.get("/")
-    print("hereee")
     assert response.status_code == 200
+
     data = response.json()
-    print(data)
+
+    assert data["status"] == "ok"
