b8df6179b0849b020e175feeab03684a07d35a20
NanezX <victor.nanej@gmail.com>
2025-11-01 18:45:27 -0400

    ruff linter fixes

--- app/crud/user.py
+++ app/crud/user.py
@@ -9,7 +9,6 @@ from app.schemas.user import UserCreate, UserUpdate
 
 
 class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
-
     def get_by_email(self, db: Session, email: str) -> Optional[User]:
         """Obtener usuario por email"""
         return (
