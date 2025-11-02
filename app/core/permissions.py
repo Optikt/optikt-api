36b3bb82a56285de20d8e2df64801751d546777d
NanezX <victor.nanej@gmail.com>
2025-11-01 20:47:58 -0400

    allow role checkers to receive a custom detail message

--- app/core/permissions.py
+++ app/core/permissions.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 from fastapi import HTTPException, status
 
@@ -8,38 +8,47 @@ from app.models.enums import UserRole
 class RoleChecker:
     """Verifica que el usuario tenga uno de los roles permitidos"""
 
-    def __init__(self, allowed_roles: List[UserRole]):
+    def __init__(
+        self,
+        allowed_roles: List[UserRole],
+    ):
         self.allowed_roles = allowed_roles
 
-    def __call__(self, user_role: str) -> bool:
+    def __call__(
+        self,
+        user_role: str,
+        detail_exception: Optional[
+            str
+        ] = "No tienes permisos suficientes para realizar esta acción",
+    ) -> bool:
         if user_role not in [role.value for role in self.allowed_roles]:
             raise HTTPException(
                 status_code=status.HTTP_403_FORBIDDEN,
-                detail="No tienes permisos suficientes para realizar esta acción",
+                detail=detail_exception,
             )
         return True
 
 
 # Decoradores predefinidos para facilitar el uso
-def require_super_admin(user_role: str) -> bool:
+def require_super_admin(user_role: str, exception: Optional[str] = None) -> bool:
     """Solo SUPER_ADMIN"""
-    return RoleChecker([UserRole.SUPER_ADMIN])(user_role)
+    return RoleChecker([UserRole.SUPER_ADMIN])(user_role, exception)
 
 
-def require_admin(user_role: str) -> bool:
+def require_admin(user_role: str, exception: Optional[str] = None) -> bool:
     """SUPER_ADMIN o ADMIN"""
-    return RoleChecker([UserRole.SUPER_ADMIN, UserRole.ADMIN])(user_role)
+    return RoleChecker([UserRole.SUPER_ADMIN, UserRole.ADMIN])(user_role, exception)
 
 
-def require_manager(user_role: str) -> bool:
+def require_manager(user_role: str, exception: Optional[str] = None) -> bool:
     """SUPER_ADMIN, ADMIN o MANAGER"""
     return RoleChecker([UserRole.SUPER_ADMIN, UserRole.ADMIN, UserRole.MANAGER])(
-        user_role
+        user_role, exception
     )
 
 
-def require_seller(user_role: str) -> bool:
+def require_seller(user_role: str, exception: Optional[str] = None) -> bool:
     """Cualquier rol excepto VIEWER"""
     return RoleChecker(
         [UserRole.SUPER_ADMIN, UserRole.ADMIN, UserRole.MANAGER, UserRole.SELLER]
-    )(user_role)
+    )(user_role, exception)
